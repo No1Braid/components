b3dcb7a47b47d3ab42f2e18ea6ee35c981507ea3
No1Braid <braidy.mail@gmail.com>
2026-04-05 23:33:22 +0100

    Update sd_card.cpp

--- sd_card/sd_card.cpp
+++ sd_card/sd_card.cpp
@@ -7,7 +7,8 @@ namespace sd_card {
 static const char *const TAG = "sd_card";
 
 void SDCardComponent::setup() {
-  ESP_LOGI(TAG, "SD Card component setup()");
+  ESP_LOGI(TAG, "SD Card setup()");
+  ESP_LOGI(TAG, "CS: %d, MOSI: %d, MISO: %d, CLK: %d", cs_pin_, mosi_pin_, miso_pin_, clk_pin_);
 }
 
 void SDCardComponent::loop() {
