f341d1df9f1b39b9f80a102885d355cfee6dcce2
No1Braid <braidy.mail@gmail.com>
2026-04-05 23:36:55 +0100

    Add setter methods for SD card pin configuration

--- sd_card/sd_card.h
+++ sd_card/sd_card.h
@@ -10,6 +10,7 @@ class SDCardComponent : public Component {
   void setup() override;
   void loop() override;
 
+  // These go RIGHT HERE:
   void set_cs_pin(int pin) { cs_pin_ = pin; }
   void set_mosi_pin(int pin) { mosi_pin_ = pin; }
   void set_miso_pin(int pin) { miso_pin_ = pin; }
