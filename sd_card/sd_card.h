8f0169c120c02e28364c0b168af7b5e167aac284
No1Braid <braidy.mail@gmail.com>
2026-04-05 23:33:03 +0100

    Update sd_card.h

--- sd_card/sd_card.h
+++ sd_card/sd_card.h
@@ -9,6 +9,17 @@ class SDCardComponent : public Component {
  public:
   void setup() override;
   void loop() override;
+
+  void set_cs_pin(int pin) { cs_pin_ = pin; }
+  void set_mosi_pin(int pin) { mosi_pin_ = pin; }
+  void set_miso_pin(int pin) { miso_pin_ = pin; }
+  void set_clk_pin(int pin) { clk_pin_ = pin; }
+
+ protected:
+  int cs_pin_;
+  int mosi_pin_;
+  int miso_pin_;
+  int clk_pin_;
 };
 
 }  // namespace sd_card
